f1d67ce410264f33f9b45ca9ecc42e7f6843b9a9
tdcoleman127 <figgeryfirehead@gmail.com>
2025-06-24 19:14:47 -0500

    Updated id for Clarity to begin tracking

--- tent.py
+++ tent.py
@@ -17,9 +17,9 @@ components.html(
     <script type="text/javascript">
         (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/YOUR_PROJECT_ID";
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/s3sokxeysl";
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "YOUR_PROJECT_ID");
+        })(window, document, "clarity", "script", "s3sokxeysl");
     </script>
     """,
     height=0,
